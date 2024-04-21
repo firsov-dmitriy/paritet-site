a693dc8aad8dcaa20bf4f41fc943034480363e2d
Dmitriy Firsov <firsov.d@uspect.ru>
2024-04-21 22:54:27 +0300

    fix: build

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,31 +1,33 @@
-import React, { FC } from "react";
+import React from "react";
 import { Box, Container, Grid, Typography } from "@mui/material";
 
-export const Footer: FC = () => (
-  <Box
-    sx={{
-      width: "100%",
-      height: "auto",
-      backgroundColor: "secondary",
-      paddingTop: "1rem",
-      paddingBottom: "1rem",
-    }}
-  >
-    <Container maxWidth="lg">
-      <Grid container direction="column" alignItems="center">
-        <Grid item xs={12}>
-          <Typography color="black" variant="h5">
-            Paritet
-          </Typography>
+function Footer() {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "auto",
+        backgroundColor: "secondary",
+        paddingTop: "1rem",
+        paddingBottom: "1rem",
+      }}
+    >
+      <Container maxWidth="lg">
+        <Grid container direction="column" alignItems="center">
+          <Grid item xs={12}>
+            <Typography color="black" variant="h5">
+              Paritet
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography color="textSecondary" variant="subtitle1">
+              {`${new Date().getFullYear()}`}
+            </Typography>
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <Typography color="textSecondary" variant="subtitle1">
-            {`${new Date().getFullYear()}`}
-          </Typography>
-        </Grid>
-      </Grid>
-    </Container>
-  </Box>
-);
+      </Container>
+    </Box>
+  );
+}
 
 export default Footer;
